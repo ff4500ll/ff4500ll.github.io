c1add618af6b77d357152b9d3c53f0372455bcdf
Trần Nguyễn Hoàng Huy <legendarysniper2412@gmail.com>
2024-12-21 16:08:16 +0700

    a

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const audio = document.getElementById('background-music');
     window.addEventListener('load', () => {
-        audio.src = 'static/mp3/DUSTCUTTER.mp3';
+        audio.src = 'static/mp3/i want to hold your hand.mp3';
         audio.load();
     });
