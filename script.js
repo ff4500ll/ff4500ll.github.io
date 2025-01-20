7f5f27f6f3ae12291a30cedf615eb3c44bcf9bcc
Trần Nguyễn Hoàng Huy <legendarysniper2412@gmail.com>
2025-01-20 19:51:07 +0700

    lamp is better

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const audio = document.getElementById('background-music');
     window.addEventListener('load', () => {
-        audio.src = 'static/mp3/亡霊 - 茶和アヤカ.mp3';
+        audio.src = 'static/mp3/Lamp - For Lovers.mp3';
         audio.load();
     });
 
