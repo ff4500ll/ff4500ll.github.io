94cfe3bdbf0e82c9de346b98a3a01d463dd993d2
Trần Nguyễn Hoàng Huy <legendarysniper2412@gmail.com>
2025-01-24 19:52:42 +0700

    Update index.html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const audio = document.getElementById('background-music');
     window.addEventListener('load', () => {
         audio.src = 'static/mp3/Lamp - For Lovers.mp3';
+        audio.volume = 0.7;
         audio.load();
     });
 
